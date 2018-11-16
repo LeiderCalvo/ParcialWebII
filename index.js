9b55a3a9b3a0a7f97328b576819a967a7df1ff26
Leider Stiven Calvo Gomez <1005705286@academico.icesi.edu.co>
2018-11-16 07:57:57 -0500

    segund commit

--- index.js
+++ index.js
@@ -3,6 +3,15 @@ const hbs = require('express-handlebars');
 const app = express();
 
 const fs = require('fs');
+var pTitulo = 0;
+var pDescrip = 0;
+var pEnlaces = 0;
+
+fs.writeFile('numeroDeVistas.txt', '', function (err) {
+    if (err) throw err;
+    console.log('Saved!');
+});
+
 
 app.use(express.static('public'));
 app.engine('handlebars', hbs());
