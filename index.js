1343f85316a7cdc99eee05eb42058df27270e3a2
Leider Stiven Calvo Gomez <1005705286@academico.icesi.edu.co>
2018-11-16 08:07:13 -0500

    tercer commit

--- index.js
+++ index.js
@@ -7,26 +7,41 @@ var pTitulo = 0;
 var pDescrip = 0;
 var pEnlaces = 0;
 
-fs.writeFile('numeroDeVistas.txt', '', function (err) {
-    if (err) throw err;
-    console.log('Saved!');
-});
-
-
 app.use(express.static('public'));
 app.engine('handlebars', hbs());
 app.set('view engine','handlebars');
 
 app.get('/', function(request,  response){
     response.render('titulo');
+    pTitulo+=1;
+    fs.writeFile('numeroDeVistas.txt',`Página Titulo: ${pTitulo} visitas
+Página descripcion: ${pDescrip} visitas
+Página enlaces: ${pEnlaces} visitas`, function (err) {
+        if (err) throw err;
+        console.log('Saved!');
+    });
 });
 
 app.get('/descripcion', function(request,  response){
     response.render('descripcion');
+    pDescrip+=1;
+    fs.writeFile('numeroDeVistas.txt',`Página Titulo: ${pTitulo} visitas
+Página descripcion: ${pDescrip} visitas
+Página enlaces: ${pEnlaces} visitas`, function (err) {
+        if (err) throw err;
+        console.log('Saved!');
+    });
 });
 
 app.get('/enlaces', function(request,  response){
     response.render('enlaces');
+    pEnlaces+=1;
+    fs.writeFile('numeroDeVistas.txt',`Página Titulo: ${pTitulo} visitas
+Página descripcion: ${pDescrip} visitas
+Página enlaces: ${pEnlaces} visitas`, function (err) {
+        if (err) throw err;
+        console.log('Saved!');
+    });
 });
 
 app.listen(5500);
